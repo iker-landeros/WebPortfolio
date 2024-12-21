065f769c1390ebfead2bcb3f862b1c1f7fe6832c
A01423214 <A01423214@tec.mx>
2024-12-20 18:11:28 -0600

    Made changes to header component (language selector)

--- WebPortfolio/src/components/Header.jsx
+++ WebPortfolio/src/components/Header.jsx
@@ -1,23 +1,42 @@
+import React, { useState } from 'react';
 import '../styles/Header.css'
 
 const Header = () => {
+    const [language, setLanguage] = useState('es');
+
+    const changeLanguage = (lang) => {
+        setLanguage(lang);
+    }
+
+    const [isLanguageOptionsVisible, setIsLanguageOptionsVisible] = useState(false);
+
+    const toggleLanguageOptions = () => {
+        setIsLanguageOptionsVisible(!isLanguageOptionsVisible);
+    };
+
     return (
         <section id="header">
         <div class="head-bg">
             <div class="headTop">
                 <p class="slogan">Aspira e inspira</p>
                 <div class="language">
-                    <div class="current-language" id="currentLanguage">
+                    <div
+                        class="current-language"
+                        id="currentLanguage"
+                        onClick={toggleLanguageOptions}    
+                    >
                         <img src="src/assets/img/0-flagMX.svg" alt="ES" class="flag"/>
                         <span>es</span>
                         <i class="fa-solid fa-chevron-down"></i>
                     </div>
-                    <div class="language-options" id="languageOptions">
-                        <div class="language-option" data-lang="EN">
-                            <img src="src/assets/img/0-flagUSA.svg" alt="EN" class="flag"/>
-                            <span>es</span>
+                    {isLanguageOptionsVisible && (
+                        <div class="language-options" id="languageOptions">
+                            <div class="language-option" data-lang="EN">
+                                <img src="src/assets/img/0-flagUSA.svg" alt="EN" class="flag"/>
+                                <span>es</span>
+                            </div>
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
             
