27a62abfd93710619eab79b28a81515939655aa0
A01423214 <A01423214@tec.mx>
2024-12-23 18:09:05 -0600

    Started Methodology section and created background

--- WebPortfolio/src/components/Methodology.jsx
+++ WebPortfolio/src/components/Methodology.jsx
@@ -10,7 +10,37 @@ const Methodology = () => {
             <h3>Mi metodología de impacto</h3>
 
             <div className="methodology-container">
+
+                <div className="methodology-item inspiration">
+                    <p>Sueños</p>
+                    <p>Comienzo</p>
+                    <p>Confianza</p>
+                    <h4>Inspiración</h4>
+                    <p>Amor</p>
+                    <p>Aspiración</p>
+                    <p>Pasión</p>
+                </div>
+
+                <div className="methodology-item education">
+                    <p>Crecimiento</p>
+                    <p>Libertad</p>
+                    <p>Aprender</p>
+                    <h4>Educación</h4>
+                    <p>Apertura</p>
+                    <p>Innovación</p>
+                    <p>Compromiso</p>
+                </div>
                 
+                <div className="methodology-item action">
+                    <p>Empatía</p>
+                    <p>Generosidad</p>
+                    <p>Perseverancia</p>
+                    <h4>Acción</h4>
+                    <p>Gratitud</p>
+                    <p>Impacto</p>
+                    <p>Colaboración</p>
+                </div>
+
             </div>
 
         </section>
