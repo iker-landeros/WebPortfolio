b922ea49eefbe694a81108d86f26671594c146c0
Iker <iker.landelao@gmail.com>
2025-03-13 22:58:29 -0600

    Minimum viable testimonies

--- WebPortfolio/src/components/Testimonies.jsx
+++ WebPortfolio/src/components/Testimonies.jsx
@@ -4,6 +4,16 @@ import '../styles/Testimonies.css';
 
 const Testimonies = () => {
     const { t } = useTranslation();
+    const testimonies = [
+        {
+            name: "Rafael Laja",
+            testimony: "Iker Landeros es un líder increíble. Su energía y creatividad me inspiraron a crear proyectos de arte con gran impacto, gracias a él me convertí en un gran Embajador Juvenil. #INSPIRE"
+        },
+        {
+            name: "Fernanda Román",
+            testimony: "Colaborar con Iker fue muy gratificante, debido a su liderazgo y habilidad de trabajo en equipo."
+        }
+    ]
     
 
     return (
@@ -11,13 +21,12 @@ const Testimonies = () => {
             <h3>{t('testimonySlider')}</h3>
             <div className="sliderFade">
                 <div className="testimonyContainer">
-                    {/* {logos.concat(logos).map((logo, index) => (
-                        <img
-                            key={index}
-                            src={logo}
-                            alt={`Logo ${index + 1}`}
-                        />
-                    ))} */}
+                    {testimonies.map((testimony, index) => (
+                        <div key={index} className="testimony">
+                            <p>{testimony.testimony}</p>
+                            <p className="testimonyName">{testimony.name}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
