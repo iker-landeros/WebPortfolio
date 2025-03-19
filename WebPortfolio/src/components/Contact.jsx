bdfe9b4593a0d91d4753fbaddceeed03750a88a9
Iker <iker.landelao@gmail.com>
2025-03-18 23:32:28 -0600

    Succesfull Integration of EmailJS

--- WebPortfolio/src/components/Contact.jsx
+++ WebPortfolio/src/components/Contact.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { useTranslation } from 'react-i18next';
 import '../styles/Contact.css';
 
 const Contact = () => {
     const { t } = useTranslation();
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_y2xen73', 'template_efqkazx', form.current, {
+                publicKey: 'e3026VJauNlGvO6OG',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
 
     return (
         <section id="contact">
@@ -12,17 +31,17 @@ const Contact = () => {
 
                 <div className="formLeft">
                     <h3>{t('contactTitle')}</h3>
-                    <form className="contactForm">
+                    <form className="contactForm" ref={form} onSubmit={sendEmail}>
                         <div className="inputContainer">
                             <div className="inputLeft">
                                 <h5>{t('contactName')}</h5>
-                                <input type="text" placeholder='...' />
+                                <input type="text" placeholder='...' name="name" />
                                 <h5>{t('contactEmail')}</h5>
-                                <input type="email" placeholder='...' />
+                                <input type="email" placeholder='...' name="email"/>
                             </div>
                             <div className="inputRight">
                                 <h5>{t('contactMessage')}</h5>
-                                <textarea placeholder='...'></textarea>
+                                <textarea placeholder='...' name="message" ></textarea>
                             </div>
                         </div>
                         <button className="submitButton" type="submit">{t('contactButton')}</button>
